bf8139e70c6150e0f56dbf3d517f556b01942116
Jiří Švec <jiri.svec@wunderman.cz>
2016-07-19 16:25:13 +0200

    adjustment PSR-4 for Monolog

---
+++ app/Monolog/DI/MonologExtension.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Monolog\DI;
+
+use Nette\PhpGenerator as Code,
+	Tracy\Debugger;
+
+/**
+ * Integrates the Monolog seamlessly into your Nette Framework application.
+ *
+ * @author Petr Horáček <petr.horacek@wunderman.cz>
+ */
+class MonologExtension extends \Kdyby\Monolog\DI\MonologExtension
+{
+
+	const TAG_HANDLER = 'monolog.handler';
+	const TAG_PROCESSOR = 'monolog.processor';
+
+	private $defaults = [
+		'handlers' => [],
+		'processors' => [],
+		'name' => 'app',
+		'hookToTracy' => TRUE,
+		// 'registerFallback' => TRUE,
+	];
+
+	public function loadConfiguration()
+	{
+		$builder = $this->getContainerBuilder();
+		$config = $this->getConfig($this->defaults);
+		$builder->parameters[$this->name] = ['name' => $config['name']];
+
+		$builder->addDefinition($this->prefix('logger'))
+			->setClass('Kdyby\Monolog\Logger', [$config['name']]);
+
+		// change channel name to priority if available
+		$builder->addDefinition($this->prefix('processor.priorityProcessor'))
+			->setClass('Kdyby\Monolog\Processor\PriorityProcessor')
+			->addTag(self::TAG_PROCESSOR);
+
+		if (!isset($builder->parameters['logDir'])) {
+			if (Debugger::$logDirectory) {
+				$builder->parameters['logDir'] = Debugger::$logDirectory;
+
+			} else {
+				$builder->parameters['logDir'] = $builder->expand('%appDir%/../log');
+			}
+		}
+
+		if (!is_dir($builder->parameters['logDir'])) {
+			@mkdir($builder->parameters['logDir']);
+		}
+
+		// handlers
+		foreach ($config['handlers'] as $handlerName => $implementation) {
+			$this->compiler->parseServices($builder, [
+				'services' => [$serviceName = $this->prefix('handler.' . $handlerName) => $implementation],
+			]);
+
+			$builder->getDefinition($serviceName)->addTag(self::TAG_HANDLER);
+		}
+
+		// processors
+		foreach ($config['processors'] as $processorName => $implementation) {
+			$this->compiler->parseServices($builder, [
+				'services' => [$serviceName = $this->prefix('processor.' . $processorName) => $implementation],
+			]);
+
+			$builder->getDefinition($serviceName)->addTag(self::TAG_PROCESSOR);
+		}
+
+		// Tracy adapter
+		$builder->addDefinition($this->prefix('adapter'))
+			->setClass('App\Monolog\Diagnostics\MonologAdapter', [$this->prefix('@logger')])
+			->addTag('logger');
+
+		if ($builder->hasDefinition('tracy.logger')) { // since Nette 2.3
+			$builder->removeDefinition($existing = 'tracy.logger');
+
+			if (method_exists($builder, 'addAlias')) { // since Nette 2.3
+				$builder->addAlias($existing, $this->prefix('adapter'));
+
+			} else { // old way of providing BC
+				$builder->addDefinition($existing)
+					->setFactory($this->prefix('@adapter'));
+			}
+		}
+	}
+
+}
--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,12 +7,7 @@ $configurator = new Nette\Configurator;
 //$configurator->setDebugMode('23.75.345.200'); // enable for your remote IP
 $configurator->enableDebugger(__DIR__ . '/../log');
 
-$configurator->setTimeZone('Europe/Prague');
 $configurator->setTempDirectory(__DIR__ . '/../temp');
-
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__.'/../vendor/others')
-	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
